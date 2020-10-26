387fc1fcbd5e66d0a87d4eb3b6987fbc632d524f
brianlunch <blynch5@tcd.ie>
2020-10-26 15:15:16 +0000

    Updates

---
+++ precache-manifest.31f5d9c4add97ac7c54e37b3467c96b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fceefab453c0a4b4f1ae696a0ed906",
+    "url": "/lockdoctor/index.html"
+  },
+  {
+    "revision": "8cceffa3bfe03a1e44fa",
+    "url": "/lockdoctor/static/css/2.6e6f38cb.chunk.css"
+  },
+  {
+    "revision": "badf4757322875c617b1",
+    "url": "/lockdoctor/static/css/main.662009f5.chunk.css"
+  },
+  {
+    "revision": "8cceffa3bfe03a1e44fa",
+    "url": "/lockdoctor/static/js/2.0bc8c36f.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/lockdoctor/static/js/2.0bc8c36f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "badf4757322875c617b1",
+    "url": "/lockdoctor/static/js/main.2875b81d.chunk.js"
+  },
+  {
+    "revision": "38026527e3461651bebe",
+    "url": "/lockdoctor/static/js/runtime-main.6797974c.js"
+  },
+  {
+    "revision": "c1b09a85b7cede9de7430cab6b5f5341",
+    "url": "/lockdoctor/static/media/headerBG.c1b09a85.png"
+  },
+  {
+    "revision": "4e0e0b8b936a510dfd2414c692e1c0f2",
+    "url": "/lockdoctor/static/media/irelandMap.4e0e0b8b.png"
+  }
+]);
